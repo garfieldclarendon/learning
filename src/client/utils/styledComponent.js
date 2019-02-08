7bd16ba9c0797bcad261a006a9d1caff277ea923
Ryan Balla <rballa@signal.co>
2019-02-07 19:01:20 -0600

    Initial dist subtree commit

--- src/client/utils/styledComponent.js
+++
@@ -1,6 +0,0 @@
-import styled from '@emotion/styled'
-
-// HOC that applies a hashed class to a component or HTML tag
-export function styledComponent(Comp, styleObject) {
-  return styled(Comp)(styleObject)
-}
