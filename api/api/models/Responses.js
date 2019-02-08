8de4c2acc40b222f39e8517388dcbbf69bf903ea
Ryan Balla <rballa@signal.co>
2019-02-08 14:31:40 -0600

    Init api

---
+++ api/api/models/Responses.js
@@ -0,0 +1,19 @@
+/**
+ * Responses.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    response: {
+      type: 'string'
+    },
+    user: {
+      type: 'string'
+    },
+  },
+
+};
